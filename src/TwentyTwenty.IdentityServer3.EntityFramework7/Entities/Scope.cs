06eafebdce9f6024c18857c4213e894a9de99ebd
Jarrod Alexander <jarroda@2020research.com>
2015-11-23 10:53:35 -0600

    Code cleanup

--- src/TwentyTwenty.IdentityServer3.EntityFramework7/Entities/Scope.cs
+++ src/TwentyTwenty.IdentityServer3.EntityFramework7/Entities/Scope.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TwentyTwenty.IdentityServer3.EntityFramework7.Entities
 {
     public class Scope
     {
         public virtual Guid Id { get; set; }
-        public virtual bool Enabled { get; set; }
-        public virtual string Name { get; set; }
-        public virtual string DisplayName { get; set; }
-        public virtual string Description { get; set; }
-        public virtual bool Required { get; set; }
-        public virtual bool Emphasize { get; set; }
-        public virtual int Type { get; set; }
-        public virtual ICollection<ScopeClaim> ScopeClaims { get; set; }
-        public virtual bool IncludeAllClaimsForUser { get; set; }
-        public virtual string ClaimsRule { get; set; }
-        public virtual bool ShowInDiscoveryDocument { get; set; }
 
+        public virtual bool Enabled { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string DisplayName { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual bool Required { get; set; }
+
+        public virtual bool Emphasize { get; set; }
+
+        public virtual int Type { get; set; }
+
+        public virtual ICollection<ScopeClaim> ScopeClaims { get; set; }
+
+        public virtual bool IncludeAllClaimsForUser { get; set; }
+
+        public virtual string ClaimsRule { get; set; }
+
+        public virtual bool ShowInDiscoveryDocument { get; set; }
     }
 }
--- src/TwentyTwenty.IdentityServer3.EntityFramework7/Entities/ScopeClaim.cs
+++ src/TwentyTwenty.IdentityServer3.EntityFramework7/Entities/ScopeClaim.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TwentyTwenty.IdentityServer3.EntityFramework7.Entities
 {
     public class ScopeClaim
     {
-        public virtual Guid Id { get; set; }    
+        public virtual Guid Id { get; set; }
+
         public virtual string Name { get; set; }
+
         public virtual string Description { get; set; }
+
         public virtual bool AlwaysIncludeInIdToken { get; set; }
+
         public virtual Scope Scope { get; set; }
     }
 }
