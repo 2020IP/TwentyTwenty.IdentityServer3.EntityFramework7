6b13374908b636bf8b7a442cca99ec85531c0399
Jarrod Alexander <jarroda@2020research.com>
2015-11-23 09:21:34 -0600

    Adding some of the Scope modeling

---
+++ src/TwentyTwenty.IdentityServer3.EntityFramework7/DbContexts/ScopeConfigurationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwentyTwenty.IdentityServer3.EntityFramework7.DbContexts
+{
+    public class ScopeConfigurationDbContext : DbContext
+    {
+        //public ScopeConfigurationDbContext()
+        //    : this(EfConstants.ConnectionName)
+        //{
+        //}
+
+        //public ScopeConfigurationDbContext(string connectionString)
+        //    : base(connectionString)
+        //{
+        //}
+
+        //public ScopeConfigurationDbContext(string connectionString, string schema)
+        //    : base(connectionString, schema)
+        //{
+        //}
+
+        //protected override void ConfigureChildCollections()
+        //{
+        //    this.Set<Scope>().Local.CollectionChanged +=
+        //        delegate (object sender, NotifyCollectionChangedEventArgs e)
+        //        {
+        //            if (e.Action == NotifyCollectionChangedAction.Add)
+        //            {
+        //                foreach (Scope item in e.NewItems)
+        //                {
+        //                    RegisterDeleteOnRemove(item.ScopeClaims);
+        //                }
+        //            }
+        //        };
+        //}
+
+        //public DbSet<Scope> Scopes { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Scope>()
+        //        .ToTable(EfConstants.TableNames.Scope, Schema)
+        //        .HasMany(x => x.ScopeClaims).WithRequired(x => x.Scope).WillCascadeOnDelete();
+
+        //    modelBuilder.Entity<ScopeClaim>().ToTable(EfConstants.TableNames.ScopeClaim, Schema);
+        //}
+    }
+}
---
+++ src/TwentyTwenty.IdentityServer3.EntityFramework7/Entities/ScopeClaim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwentyTwenty.IdentityServer3.EntityFramework7.Entities
+{
+    public class ScopeClaim
+    {
+        public virtual Guid Id { get; set; }    
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual bool AlwaysIncludeInIdToken { get; set; }
+        public virtual Scope Scope { get; set; }
+    }
+}
