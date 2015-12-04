ffb83810defe752c926abc7b60ca8a6132b2751d
Jarrod Alexander <jarroda@2020research.com>
2015-12-04 09:33:02 -0600

    Fixed bug with the aspnet DI container's service scope not playing nicely with IS3's autofac scope lifetime.

--- src/TwentyTwenty.IdentityServer3.EntityFramework7/Extensions/IdentityServerServiceFactoryExtensions.cs
+++ src/TwentyTwenty.IdentityServer3.EntityFramework7/Extensions/IdentityServerServiceFactoryExtensions.cs
@@ -1,62 +1,36 @@
-﻿using IdentityServer3.Core.Configuration;
-using IdentityServer3.Core.Services;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
-using TwentyTwenty.IdentityServer3.EntityFramework7.DbContexts;
-using TwentyTwenty.IdentityServer3.EntityFramework7.Services;
-using TwentyTwenty.IdentityServer3.EntityFramework7.Stores;
+using TwentyTwenty.IdentityServer3.EntityFramework7;
+using TwentyTwenty.IdentityServer3.EntityFramework7.Extensions;
 
-namespace TwentyTwenty.IdentityServer3.EntityFramework7.Extensions
+namespace IdentityServer3.Core.Configuration
 {
     public static class IdentityServerServiceFactoryExtensions
     {
-        public static IdentityServerServiceFactory RegisterOperationalStores(this IdentityServerServiceFactory factory, IServiceProvider services)
+        public static EntityFrameworkOptions ConfigureEntityFramework(this IdentityServerServiceFactory factory, IServiceProvider services)
         {
             if (factory == null) throw new ArgumentNullException("factory");
             if (services == null) throw new ArgumentNullException("sevices");
 
-            factory.Register(services.GetRegistration<OperationalContext>());
+            // Assert scope factory is registered
+            services.GetRequiredService<IServiceScopeFactory>();
+            factory.Register(new Registration<IServiceScopeFactory>(r => services.GetService<IServiceScopeFactory>()));
 
-            factory.AuthorizationCodeStore = services.GetRegistration<IAuthorizationCodeStore>();
-            factory.TokenHandleStore = services.GetRegistration<ITokenHandleStore>();
-            factory.ConsentStore = services.GetRegistration<IConsentStore>();
-            factory.RefreshTokenStore = services.GetRegistration<IRefreshTokenStore>();
-
-            return factory;
+            return new EntityFrameworkOptions(factory, services);
         }
         
-        public static IdentityServerServiceFactory RegisterClientStore<TKey, TContext>(this IdentityServerServiceFactory factory, IServiceProvider services)
-            where TKey : IEquatable<TKey>
-            where TContext : ClientConfigurationContext<TKey>
+        internal static IdentityServerServiceFactory RegisterScopedAspnetService<T>(this IdentityServerServiceFactory factory)
+            where T : class
         {
-            if (factory == null) throw new ArgumentNullException("factory");
-            if (services == null) throw new ArgumentNullException("services");
-
-            factory.Register(new Registration<ClientConfigurationContext<TKey>>(r => services.GetRequiredService<TContext>()));
-            
-            factory.ClientStore = services.GetRegistration<IClientStore>();
-            factory.CorsPolicyService = new Registration<ICorsPolicyService, ClientConfigurationCorsPolicyService<TKey>>();
-
-            return factory;
+            return factory.RegisterScopedAspnetService<T, T>();
         }
 
-        public static IdentityServerServiceFactory RegisterScopeStore<TKey, TContext>(this IdentityServerServiceFactory factory, IServiceProvider services)
-            where TKey : IEquatable<TKey>
-            where TContext : ScopeConfigurationContext<TKey>
+        internal static IdentityServerServiceFactory RegisterScopedAspnetService<TClass, TAs>(this IdentityServerServiceFactory factory)
+            where TClass : TAs
+            where TAs : class
         {
-            if (factory == null) throw new ArgumentNullException("factory");
-            if (services == null) throw new ArgumentNullException("services");
-
-            factory.Register(new Registration<ScopeConfigurationContext<TKey>>(r => services.GetRequiredService<TContext>()));
-
-            factory.ScopeStore = services.GetRegistration<IScopeStore>();
-
+            factory.Register(new Registration<TAs>(r => r.Resolve<IServiceScopeFactory>().GetScopedService<TClass>()));
             return factory;
-        }
-
-        private static Registration<T> GetRegistration<T>(this IServiceProvider services) where T : class
-        {
-            return new Registration<T>(resolver => services.GetRequiredService<T>());
         }
     }
 }
